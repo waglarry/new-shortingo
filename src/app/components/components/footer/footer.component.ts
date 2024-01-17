510e6ce9556080d25ba4fb8efdb1e4801f6db41f
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2024-01-17 07:48:13 +0000

    Update footer with working contact form

--- src/app/components/components/footer/footer.component.ts
+++ src/app/components/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_jlc54lk', 'template_8z8orm8', e.target as HTMLFormElement, '4MHutAdVwgaCx2KQa')
+      .then((result: EmailJSResponseStatus) => {
+        alert('Message is successfully sent!')
+        console.log(result.text);
+        
+      }, (error) => {
+        console.log(error.text);
+        alert(error?.text)
+      });
+  }
 
 }
