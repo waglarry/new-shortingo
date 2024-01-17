f845d0c8def2f5b6747e7f4f6d41c2971955738d
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2024-01-17 11:24:07 +0000

    Create environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  BaseUrl: 'https://shortingo-api.onrender.com/api',
+
+  // Email Js Keys
+  SERVICE_ID: 'service_jlc54lk',
+  TEMPLATE_ID: 'template_8z8orm8',
+  PUBLIC_KEY: '4MHutAdVwgaCx2KQa',
+};
