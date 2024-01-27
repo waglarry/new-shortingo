142bfd40af92fd9de9296926fb03c44b6fc8ce1d
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2024-01-27 17:19:16 +0000

    Add toastr functions to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideToastr(),
+    provideAnimations(),
+  ],
 };
